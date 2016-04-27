9c2447cc8a15a091c94c3423da3a556467c0786a
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-04-27 09:39:08 -0500

    Installation de gem 'jquery-datatables-rails', '~> 3.3.0' Seeder de Modulo de Barbershops

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Barbershop
+30.times do
+ barbershop = BarberShop.create(
+   name: Faker::Address.street_name,
+   code: Faker::Code.isbn,
+   business_name: Faker::Company.name,
+   nit: Faker::Code.isbn,
+   address: Faker::Address.street_address,
+   city: Faker::Address.city,
+   phone: Faker::Company.duns_number,
+   owner: Faker::Name.name,
+   contact_name: Faker::Name.name, 
+   contact_phone1: Faker::Company.duns_number,
+   contact_phone2: Faker::Company.duns_number,
+   contact_email: Faker::Internet.email,
+   enable: 't'
+ )
+end
