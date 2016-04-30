03cd54df393491c15cb1804843c157d839450442
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-04-30 12:44:02 -0500

    seeder de mis modelos generados

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,35 @@
    city: Faker::Address.city,
    phone: Faker::Company.duns_number,
    owner: Faker::Name.name,
-   contact_name: Faker::Name.name, 
+   contact_name: Faker::Name.name,
    contact_phone1: Faker::Company.duns_number,
    contact_phone2: Faker::Company.duns_number,
    contact_email: Faker::Internet.email,
    enable: 't'
  )
+
+ service = Service.create(
+   name: Faker::Name.name,
+   code: Faker::Code.isbn,
+   description: Faker::Lorem.sentence,
+   price: Faker::Commerce.price,
+   barber_shop_id: '1',
+   enable: 't'
+ )
+
+ line = Line.create(
+   name: Faker::Name.name,
+   code: Faker::Code.isbn,
+   description: Faker::Lorem.sentence,
+   enable: 't'
+ )
+
+ subline = Subline.create(
+   name: Faker::Name.name,
+   code: Faker::Code.isbn,
+   description: Faker::Lorem.sentence,
+   line_id: '1',
+   enable: 't'
+ )
+
 end
